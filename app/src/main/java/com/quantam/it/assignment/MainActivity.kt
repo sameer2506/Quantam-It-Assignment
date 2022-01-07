fa235cd890e43fd903d8aa4144b880fe49faffc1
Sameer <sameerkr679@gmail.com>
2022-01-07 23:22:10 +0530

    sign in activity design

--- app/src/main/java/com/quantam/it/assignment/MainActivity.kt
+++ app/src/main/java/com/quantam/it/assignment/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.quantam.it.assignment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.quantam.it.assignment.ui.auth.SignInActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, SignInActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/quantam/it/assignment/ui/auth/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.quantam.it.assignment.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.quantam.it.assignment.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
