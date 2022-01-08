d0532e60083532208baede192c5bd28351d7affe
Sameer <sameerkr679@gmail.com>
2022-01-08 10:28:25 +0530

    navigation activity created for home

--- app/src/main/java/com/quantam/it/assignment/MainActivity.kt
+++ app/src/main/java/com/quantam/it/assignment/MainActivity.kt
@@ -4,12 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.quantam.it.assignment.ui.auth.activity.AuthenticationActivity
+import com.quantam.it.assignment.ui.home.HomeActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startActivity(Intent(this, AuthenticationActivity::class.java))
+        startActivity(Intent(this, HomeActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/quantam/it/assignment/ui/home/fragments/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.quantam.it.assignment.ui.home.fragments.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
